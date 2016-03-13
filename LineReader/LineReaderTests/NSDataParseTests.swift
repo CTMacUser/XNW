dfc9374efed84c40553f5dba8c28c9e988c700e1
Daryle Walker <dwalker07@yahoo.com>
2016-03-12 22:15:55 -0500

    Add tests for NSData-to-parse-tree converter property.

---
+++ LineReader/LineReaderTests/NSDataParseTests.swift
@@ -0,0 +1,55 @@
+//
+//  NSDataParseTests.swift
+//  LineReader
+//
+//  Created by Daryle Walker on 3/12/16.
+//  Copyright © 2016 Daryle Walker. All rights reserved.
+//
+
+import XCTest
+import Foundation
+@testable import LineReader
+
+
+class NSDataParseTests: XCTestCase {
+
+    var noData: NSData?
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        noData = NSData()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    // Use a NSData object without stored data.
+    func testEmptyData() {
+        let node = noData!.parseTree
+        XCTAssertNil(node)
+
+        //self.measureBlock { _ = self.noData!.parseTree }
+    }
+
+    // Convert a non-empty NSData object.
+    func testBasicData() {
+        func parseDepth<T>(node: ParseNode<T>) -> Int {
+            return 1 + node.next.values.reduce(0, combine: { return max($0, parseDepth($1)) })
+        }
+
+        let dataString = "Hello world"
+        let data = (dataString as NSString).dataUsingEncoding(NSASCIIStringEncoding)
+        let parse = data!.parseTree
+        XCTAssertNotNil(parse)
+        XCTAssertEqual(parseDepth(parse!), dataString.utf8.count)
+        let terms = parse!.terminals()
+        XCTAssertEqual(terms.count, 1)
+        XCTAssertEqual(terms[0].count, dataString.utf8.count)
+        let termData = NSData(bytes: terms[0], length: terms[0].count * strideof(terms[0].dynamicType.Element.self))
+        XCTAssert(termData.isEqualToData(data!))
+    }
+
+}
