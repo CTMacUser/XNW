a05c2a5d278682919fb2bd35e46071630732baf1
Daryle Walker <dwalker07@yahoo.com>
2017-01-08 06:41:58 -0500

    Refactor and specialize document code.
    Change to a better-named document class. Move its window and view
    controller code to new/better-named classes. Move the window and view
    interface from the main storyboard to a new one.
    
    Expand the attributes of the main document type in the information
    property list.

--- XNW/XNW/EditableMessageViewController.swift
+++ XNW/XNW/EditableMessageViewController.swift
@@ -1,19 +1,19 @@
 //
-//  ViewController.swift
+//  EditableMessageViewController.swift
 //  XNW
 //
-//  Created by Daryle Walker on 1/1/17.
+//  Created by Daryle Walker on 1/8/17.
 //  Copyright © 2017 Daryle Walker. All rights reserved.
 //
 
 import Cocoa
 
-class ViewController: NSViewController {
+
+class EditableMessageViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do view setup here.
     }
 
     override var representedObject: Any? {
@@ -22,6 +22,4 @@ class ViewController: NSViewController {
         }
     }
 
-
 }
-
---
+++ XNW/XNW/EditableMessageWindowController.swift
@@ -0,0 +1,24 @@
+//
+//  EditableMessageWindowController.swift
+//  XNW
+//
+//  Created by Daryle Walker on 1/7/17.
+//  Copyright © 2017 Daryle Walker. All rights reserved.
+//
+
+import Cocoa
+
+
+class EditableMessageWindowController: NSWindowController {
+
+    enum Names {
+        static let storyboard = "EditableMessageWindow"
+    }
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
